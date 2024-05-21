8c783961e58a7ee903c67bc62037bbf474b200e0
manoj domala <manojdomala@gmail.com>
2024-05-21 22:26:23 +0530

    new version

--- python fundamentals_003.py
+++ python fundamentals_003.py
@@ -1,1 +1,107 @@
-print ("hello")
+Name = "manoj domala"
+size = len(Name)
+print (Name[-size])
+print (size)
+
+# String Slicing
+
+name = " manoj kumar"
+name [0:6]
+size = len(name)
+name[3:size]
+
+
+# capitalize
+name = "manoj"
+print (name.capitalize())
+
+# title
+
+name= "manoj is name"
+print(name.title())
+
+# upper
+name = "i'm doing good"
+print (name.upper())
+
+# lower
+
+name = "I'M DOING GOOD"
+print (name.lower())
+
+# find
+
+name = "I'M DOING GOOD"
+print (name.find("O"))
+
+
+# find
+
+name ='manoj is my name'
+print (name.find('is'))
+
+# count
+
+name = "is this good for us"
+print (name.count("o"))
+
+# index 
+name = "is this good for us"
+print (name.index ("f"))
+
+# replace
+
+name = "is this good for us"
+print (name.replace("us","me"))
+
+# split
+
+name = "is this good for us"
+lst=name.split()
+print (lst)
+
+# islower
+name = "is this good for us"
+lst=name.islower()
+print (lst)
+
+# isupper
+name = "is this good for us"
+lst=name.isupper()
+print (lst)
+
+
+# isnumeric
+name = "is this good for us"
+lst=name.isnumeric()
+print (lst)
+
+# isalpha
+name = "is this good for us"
+lst=name.isalpha()
+print (lst)
+
+# formatting
+
+name = "manoj"
+age = 25
+
+print ("my name is {} and my age is {}" .format(name,age))
+
+
+# Concatenation
+
+first = 2
+second = 15
+
+print (first+second)
+
+#  Vowels
+text = "my name is manoj and my age is", 25
+
+if (text='a' or text = 'e'):
+    print(text)
+
+
+
+
